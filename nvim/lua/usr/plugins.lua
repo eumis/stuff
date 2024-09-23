4f23d78774b6b6deed2e0cc3168330f43a3995ae
eumis <eumisdev@proton.me>
2024-09-23 16:47:49 +0200

    snippets

--- nvim/lua/usr/plugins.lua
+++ nvim/lua/usr/plugins.lua
@@ -67,18 +67,19 @@ function M.setup_lazy()
         'nvim-neotest/neotest',
         'nvim-neotest/neotest-python',
         'Issafalcon/neotest-dotnet',
+        --
+        --snippets
+        'L3MON4D3/LuaSnip',
 
         --autocomplete
+        'hrsh7th/nvim-cmp',
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
-        'hrsh7th/nvim-cmp',
+        'saadparwaiz1/cmp_luasnip',
         'windwp/nvim-autopairs',
 
-        --snippets
-        'saadparwaiz1/cmp_luasnip',
-        'L3MON4D3/LuaSnip',
         --Plug 'puremourning/vimspector'
 
         --visual
