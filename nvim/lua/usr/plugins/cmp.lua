296995073654e09240c5c3235dcbac975ecfcded
eumis <eumisdev@proton.me>
2024-11-14 17:59:07 +0100

    cmp is lazy

--- nvim/lua/usr/plugins/cmp.lua
+++ nvim/lua/usr/plugins/cmp.lua
@@ -1,7 +1,7 @@
 return {
     {
         'hrsh7th/nvim-cmp',
-        lazy = false,
+        lazy = true,
         dependencies = {
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
