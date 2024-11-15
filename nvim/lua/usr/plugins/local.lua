9138c7e316ce0540e01c188b6be792717732b221
eumis <eumisdev@proton.me>
2024-11-15 17:37:55 +0100

    fixed local

--- nvim/lua/usr/plugins/local.lua
+++ nvim/lua/usr/plugins/local.lua
@@ -5,7 +5,7 @@ return {
         dir = '~/stuff_local/plugins/nvwd',
         cond = function()
             local Path = require('plenary.path')
-            return Path:new(Path.path.home .. '/stuff_local/plugins/nvwd')
+            return Path:new(Path.path.home .. '/stuff_local/plugins/nvwd'):exists()
         end,
         config = function()
             require('nvwd')
