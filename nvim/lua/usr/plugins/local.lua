f4b900b95e3bd4446ee55cf5423762f51f36f962
eumis <eumisdev@proton.me>
2024-11-15 17:04:59 +0100

    fixed local plugins

--- nvim/lua/usr/plugins/local.lua
+++ nvim/lua/usr/plugins/local.lua
@@ -3,6 +3,10 @@ return {
         'nvwd',
         event = 'VeryLazy',
         dir = '~/stuff_local/plugins/nvwd',
+        cond = function()
+            local Path = require('plenary.path')
+            return Path:new(Path.path.home .. '/stuff_local/plugins/nvwd')
+        end,
         config = function()
             require('nvwd')
         end
