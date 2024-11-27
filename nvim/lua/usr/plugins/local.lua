6f71bcfbf8a93bc5b02c8f150a7d80480ad53dfe
eumis <eumisdev@proton.me>
2024-11-27 15:03:35 +0100

    updates

--- nvim/lua/usr/plugins/local.lua
+++ nvim/lua/usr/plugins/local.lua
@@ -1,26 +1,29 @@
+local get_cond = function(plugin)
+    return function()
+        local Path = require('plenary.path')
+        return Path:new(Path.path.home .. '/data/plugins/' .. plugin):exists()
+    end
+end
+
+local get_config = function(plugin)
+    return function()
+        require(plugin)
+    end
+end
+
 return {
     {
         'nvwd',
         event = 'VeryLazy',
         dir = '~/data/plugins/nvwd',
-        cond = function()
-            local Path = require('plenary.path')
-            return Path:new(Path.path.home .. '/data/plugins/nvwd'):exists()
-        end,
-        config = function()
-            require('nvwd')
-        end
+        cond = get_cond('nvwd'),
+        config = get_config('nvwd')
     },
     {
         'boil',
         event = 'VeryLazy',
         dir = '~/data/plugins/boil',
-        cond = function()
-            local Path = require('plenary.path')
-            return Path:new(Path.path.home .. '/data/plugins/nvwd'):exists()
-        end,
-        config = function()
-            require('nvwd')
-        end
+        cond = get_cond('boil'),
+        config = get_config('boil')
     },
 }
