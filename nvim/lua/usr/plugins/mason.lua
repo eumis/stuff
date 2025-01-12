fc0ebe09d96218e9ce61565ec1650dd38a80c36e
eumis <eumisdev@proton.me>
2025-01-12 16:49:25 +0100

    updates to terminal keys

--- nvim/lua/usr/plugins/mason.lua
+++ nvim/lua/usr/plugins/mason.lua
@@ -35,7 +35,8 @@ return {
                 -- lsp
                 'lua-language-server',
                 'basedpyright',
-                {'omnisharp', version = 'v1.39.11'},
+                -- {'omnisharp', version = 'v1.39.11'},
+                'omnisharp',
                 'lemminx',
                 'json-lsp',
                 'marksman',
--- nvim/lua/usr/terminal.lua
+++ nvim/lua/usr/terminal.lua
@@ -2,19 +2,20 @@ local M = {}
 
 local state = {
     buf = -1,
-    floating = {
-        float_win = -1,
-        split_win = -1,
-        bottom_win = -1
-    }
+    win = -1
 }
 
-local function create_terminal_window(win_config)
+local function toggle_terminal_window(win_config)
+    if vim.api.nvim_win_is_valid(state.win) then
+        vim.api.nvim_win_hide(state.win)
+        return
+    end
+
     if not vim.api.nvim_buf_is_valid(state.buf) then
         state.buf = vim.api.nvim_create_buf(false, true)
     end
 
-    vim.api.nvim_open_win(state.buf, true, win_config)
+    state.win = vim.api.nvim_open_win(state.buf, true, win_config)
 
     if vim.bo[state.buf].buftype ~= "terminal" then
         vim.cmd.terminal()
@@ -26,7 +27,7 @@ function M.float_terminal()
     local height = math.floor(vim.o.lines * 0.8)
     local col = math.floor((vim.o.columns - width) / 2)
     local row = math.floor((vim.o.lines - height) / 2)
-    create_terminal_window({
+    toggle_terminal_window({
         relative = "editor",
         width = width,
         height = height,
@@ -38,14 +39,14 @@ function M.float_terminal()
 end
 
 function M.split_terminal()
-    create_terminal_window({
+    toggle_terminal_window({
         split = "right",
         style = "minimal", -- No borders or extra UI elements
     })
 end
 
 function M.bottom_terminal()
-    create_terminal_window({
+    toggle_terminal_window({
         split = "bottom",
         style = "minimal", -- No borders or extra UI elements
     })
