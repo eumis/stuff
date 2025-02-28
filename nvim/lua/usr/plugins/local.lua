ca9624fa5a38c3094fd5cd7d086dba2d452751fc
eumis <eug.mis@protonmail.com>
2025-02-28 17:06:06 +0100

    custom plugins refactored

--- nvim/lua/usr/plugins/local.lua
+++ nvim/lua/usr/plugins/local.lua
@@ -1,63 +1,30 @@
-local get_cond = function(plugin)
-    return function()
-        local Path = require('plenary.path')
-        return Path:new(Path.path.home .. '/data/plugins/' .. plugin):exists()
-    end
-end
+local Path = require('plenary.path')
+local plugins_root = '~/data/plugins'
 
-local get_config = function(plugin)
-    return function()
-        require(plugin)
-    end
+---@param name
+---@param options? {root?: string, config: fun()}
+local function custom_plugin(name, options)
+    options = options or {}
+    options.root = options.root or plugins_root
+    options.config = options.config or function() require(name) end
+    return {
+        name,
+        event = 'VeryLazy',
+        dir = options.root .. '/' .. name,
+        cond = function()
+            return Path:new(vim.fn.expand(plugins_root), name):exists()
+        end,
+        config = function()
+            require(name)
+        end
+    }
 end
 
 return {
-    {
-        'util',
-        event = 'VeryLazy',
-        dir = '~/data/plugins/util',
-        cond = get_cond('util'),
-        config = get_config('util')
-    },
-    {
-        'nvwd',
-        event = 'VeryLazy',
-        dir = '~/data/plugins/nvwd',
-        cond = get_cond('nvwd'),
-        config = get_config('nvwd')
-    },
-    {
-        'boil',
-        event = 'VeryLazy',
-        dir = '~/data/plugins/boil',
-        cond = get_cond('boil'),
-        config = get_config('boil')
-    },
-    {
-        'luamd',
-        event = 'VeryLazy',
-        dir = '~/data/plugins/luamd',
-        cond = get_cond('luamd'),
-        config = function()
-            require('luamd').setup()
-        end
-    },
-    {
-        'auto',
-        event = 'VeryLazy',
-        dir = '~/data/plugins/auto',
-        cond = get_cond('auto'),
-        config = function()
-            require('auto').setup()
-        end
-    },
-    {
-        'task',
-        event = 'VeryLazy',
-        dir = '~/data/plugins/task',
-        cond = get_cond('task'),
-        -- config = function()
-        --     require('auto').setup()
-        -- end
-    },
+    custom_plugin('util'),
+    custom_plugin('nvwd'),
+    custom_plugin('luamd', { config = function() require('auto').setup() end }),
+    custom_plugin('auto', { config = function() require('auto').setup() end }),
+    custom_plugin('task'),
+    custom_plugin('nvim', { root = '~/data' })
 }
