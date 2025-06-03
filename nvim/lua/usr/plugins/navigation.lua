44a54c410073d2f99712c4737aafe13e48378dc6
eumis <eumisdev@proton.me>
2025-06-03 14:44:50 +0200

    updates

--- nvim/lua/usr/plugins/navigation.lua
+++ nvim/lua/usr/plugins/navigation.lua
@@ -102,6 +102,13 @@ return {
         -- branch = "harpoon2",
         -- enabled = false,
         dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("harpoon").setup {
+                menu = {
+                    width = vim.api.nvim_win_get_width(0) - 20,
+                }
+            }
+        end
         -- config = function()
         --     local harpoon = require("harpoon")
         --     harpoon:setup({
