c6b50c1f1fd83cb6c8b8611fddc36b2ad9667882
eumis <eumisdev@proton.me>
2025-07-14 10:26:00 +0200

    updates

--- nvim/lua/usr/plugins/common.lua
+++ nvim/lua/usr/plugins/common.lua
@@ -2,5 +2,16 @@ return {
     { 'nvim-lua/plenary.nvim', lazy = false },
     { 'nvim-neotest/nvim-nio', lazy = false },
     { "eumis/tasks.nvim",      branch = "dev", lazy = true },
-    { "eumis/wdconfig.nvim",   branch = "dev", event = "VeryLazy", config = function() require("wdconfig").load_cwd() end }
+    {
+        "eumis/wdconfig.nvim",
+        branch = "dev",
+        event = "VeryLazy",
+        config = function()
+            local Path = require "plenary.path"
+            local home = vim.fn.expand('~')
+            local user_config = Path:new(home, 'data', 'user.lua')
+            require("wdconfig").load(user_config)
+            require("wdconfig").load_cwd()
+        end
+    }
 }
