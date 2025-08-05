6d14de918b507d6299a5af2eb6e173f45d678e67
eumis <eumisdev@proton.me>
2025-08-05 09:45:59 +0200

    updates

--- nvim/lua/usr/plugins/cmp.lua
+++ nvim/lua/usr/plugins/cmp.lua
@@ -6,24 +6,24 @@ return {
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
-            'saadparwaiz1/cmp_luasnip',
+            -- 'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-nvim-lsp-signature-help'
         },
         config = function()
             local cmp = require('cmp')
             cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
-                    end,
-                },
+                -- snippet = {
+                --     expand = function(args)
+                --         require('luasnip').lsp_expand(args.body)
+                --     end,
+                -- },
                 mapping = cmp.mapping.preset.insert(require('usr.keymap').get_cmp_mapping()),
                 sources = {
                     { name = 'nvim_lsp_signature_help' },
                     { name = 'lazydev' },
                     { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
+                    -- { name = 'luasnip' },
                     { name = 'path' }
                 }
             })
