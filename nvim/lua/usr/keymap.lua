3c9b3288212d42b19c8afdfe20f7ca08c30acefa
eumis <eumisdev@proton.me>
2025-09-23 20:33:09 +0200

    lsp updates

--- nvim/lua/usr/keymap.lua
+++ nvim/lua/usr/keymap.lua
@@ -184,28 +184,22 @@ vim.keymap.set("n", "<C-a><C-b>", function() require("runb.view").previous_tab()
 
 -- cmp
 function M.get_cmp_mapping()
-    local cmp = require('cmp')
+    local cmp = require("cmp")
     return {
-        -- ['<C-p>'] = cmp.mapping.select_prev_item(),
-        -- ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-u>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete({}),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-        },
-        ['<Tab>'] = function(fallback)
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete({}),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<Tab>"] = function(fallback)
             if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
             else
                 fallback()
             end
         end,
-        ['<S-Tab>'] = function(fallback)
+        ["<S-Tab>"] = function(fallback)
             if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
             else
                 fallback()
             end
--- nvim/lua/usr/plugins/cmp.lua
+++ nvim/lua/usr/plugins/cmp.lua
@@ -1,75 +1,33 @@
 return {
-    -- {
-    --     'hrsh7th/nvim-cmp',
-    --     lazy = true,
-    --     dependencies = {
-    --         'hrsh7th/cmp-buffer',
-    --         'hrsh7th/cmp-path',
-    --         'hrsh7th/cmp-cmdline',
-    --         -- 'saadparwaiz1/cmp_luasnip',
-    --         'hrsh7th/cmp-nvim-lsp',
-    --         'hrsh7th/cmp-nvim-lsp-signature-help'
-    --     },
-    --     config = function()
-    --         local cmp = require('cmp')
-    --         cmp.setup({
-    --             -- snippet = {
-    --             --     expand = function(args)
-    --             --         require('luasnip').lsp_expand(args.body)
-    --             --     end,
-    --             -- },
-    --             mapping = cmp.mapping.preset.insert(require('usr.keymap').get_cmp_mapping()),
-    --             sources = {
-    --                 { name = 'nvim_lsp_signature_help' },
-    --                 { name = 'lazydev' },
-    --                 { name = 'nvim_lsp' },
-    --                 -- { name = 'luasnip' },
-    --                 { name = 'path' }
-    --             }
-    --         })
-    --     end
-    -- },
     {
-        "saghen/blink.cmp",
-        version = "1.*",
-        ---@module "blink.cmp"
-        ---@type blink.cmp.Config
-        opts = {
-            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-            -- 'super-tab' for mappings similar to vscode (tab to accept)
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- All presets have the following mappings:
-            -- C-space: Open menu or open docs if already open
-            -- C-n/C-p or Up/Down: Select next/previous item
-            -- C-e: Hide menu
-            -- C-k: Toggle signature help (if signature.enabled = true)
-            --
-            -- See :h blink-cmp-config-keymap for defining your own keymap
-            keymap = { preset = 'default' },
-            signature = { enabled = true },
-            appearance = {
-                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                -- Adjusts spacing to ensure icons are aligned
-                nerd_font_variant = 'mono'
-            },
-
-            -- (Default) Only show the documentation popup when manually triggered
-            completion = { documentation = { auto_show = true } },
-
-            -- Default list of enabled providers defined so that you can extend it
-            -- elsewhere in your config, without redefining it, due to `opts_extend`
-            sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
-            },
-
-            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-            --
-            -- See the fuzzy documentation for more information
-            fuzzy = { implementation = "prefer_rust_with_warning" }
-        }
+        'hrsh7th/nvim-cmp',
+        lazy = true,
+        dependencies = {
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            -- 'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-signature-help'
+        },
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                -- snippet = {
+                --     expand = function(args)
+                --         require('luasnip').lsp_expand(args.body)
+                --     end,
+                -- },
+                mapping = cmp.mapping.preset.insert(require('usr.keymap').get_cmp_mapping()),
+                sources = {
+                    { name = 'nvim_lsp_signature_help' },
+                    { name = 'lazydev' },
+                    { name = 'nvim_lsp' },
+                    -- { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'buffer' }
+                }
+            })
+        end
     }
 }
