62da69b551da190ef93791ae1ad3d8f71bedbe1a
Eugen Misievich <eugenmisievich@coherentsolutions.com>
2025-10-30 13:23:10 +0100

    added btop4win

--- nopm/btop.sh
+++ nopm/btop.sh
@@ -3,21 +3,25 @@
 cleanup() {
     rm -rf ./btop || echo
     rm -f btop.tbz || echo
-}
-
-get_file_name() {
-    arch=$(get_architecture)
-
-    echo "btop-$arch-linux-musl.tbz"
+    rm -f btop4win-x64.zip || echo
 }
 
 install() {
     local version="$1"
+    local os=$(get_os)
+    local arch=$(get_architecture)
 
     cleanup
-    curl -LRs "https://github.com/aristocratos/btop/releases/download/v$version/$(get_file_name)" -o btop.tbz
-    tar -xf btop.tbz
-    sudo cp btop/bin/btop "/usr/local/bin/btop" -f
+    if [[ $os == "linux" ]]; then
+        curl -LRs "https://github.com/aristocratos/btop/releases/download/v$version/btop-$arch-linux-musl.tbz" -o btop.tbz
+        tar -xf btop.tbz
+        sudo cp btop/bin/btop "/usr/local/bin/btop" -f
+    else
+        curl -LRs "https://github.com/aristocratos/btop4win/releases/download/v$version/btop4win-LHM-x64.zip" -o "btop.zip"
+        unzip btop.zip
+        mv btop4win/btop4win.exe btop4win/btop.exe
+        mv btop4win "/c/tools/btop" -f
+    fi
     cleanup
 }
 
@@ -32,9 +36,19 @@ get_installed_version() {
 }
 
 get_latest_version() {
-    curl -s https://api.github.com/repos/aristocratos/btop/releases/latest | jq -r '.tag_name' | tr -d 'v'
+    local os=$(get_os)
+    if [[ $os == "linux" ]]; then
+        curl -s https://api.github.com/repos/aristocratos/btop/releases/latest | jq -r '.tag_name' | tr -d 'v'
+    else
+        curl -s https://api.github.com/repos/aristocratos/btop4win/releases/latest | jq -r '.tag_name' | tr -d 'v'
+    fi
 }
 
 uninstall() {
-    sudo rm "/usr/local/bin/btop" || echo
+    local os=$(get_os)
+    if [[ $os == "linux" ]]; then
+        sudo rm "/usr/local/bin/btop" || echo
+    else
+        rm -rf "/c/tools/btop" || echo
+    fi
 }
