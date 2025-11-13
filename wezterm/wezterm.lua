2f3a5f99d205dcf1be201b954cb822c61b1479b7
Eugen Misievich <eugenmisievich@coherentsolutions.com>
2025-11-13 14:39:04 +0100

    updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,20 +6,18 @@ config.color_scheme = 'OneHalfDark'
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
+config.front_end = "WebGpu"
 
 config.font = wezterm.font_with_fallback({
-    { family = "Hack Nerd Font Mono", weight = "Regular", stretch = "Normal", style = "Normal" },
-    'CodeNewRoman Nerd Font',
-    'FiraCode'
+    "CodeNewRoman Nerd Font Mono"
 })
 config.font_size = 11
 
 local shells = nil
 if string.find(wezterm.target_triple, "windows") then
     shells = {
-        { key = 'n', args = 'C:\\Program Files\\Git\\usr\\bin\\bash.exe' },
-        { key = 'l', args = 'C:\\WINDOWS\\system32\\wsl.exe -d Ubuntu' },
-        { key = 'p', args = 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+        { key = 'g', args = 'C:\\Program Files\\Git\\bin\\bash.exe' },
+        { key = 'n', args = 'wsl -d Ubuntu-24.04' },
     }
     config.font_size = 9
     config.window_background_gradient = {
@@ -41,8 +39,8 @@ else
             source = { File = 'stuff/wezterm/term_back.png' },
             hsb = {
                 brightness = 0.03, -- Set brightness to 50%
-                hue = 1.0, -- No change to hue
-                saturation = 1.0, -- No change to saturation
+                hue = 1.0,         -- No change to hue
+                saturation = 1.0,  -- No change to saturation
             },
             vertical_align = "Middle",
             horizontal_align = "Right"
