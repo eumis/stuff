5ff4b366b7324dbe235333c02c03d0d605a98420
eumis <eumisdev@proton.me>
2025-11-17 10:34:58 +0100

    diffview

--- nvim/lua/usr/plugins/git.lua
+++ nvim/lua/usr/plugins/git.lua
@@ -1,21 +1,21 @@
 return {
-    -- {
-    --     "sindrets/diffview.nvim",
-    --     lazy = true,
-    --     event = "VeryLazy",
-    --     config = function()
-    --         require("diffview").setup({
-    --             keymaps = require("usr.keymap").get_diffview_keymaps(),
-    --             file_panel = {
-    --                 win_config = {
-    --                     position = "left",
-    --                     width = 70,
-    --                     win_opts = {},
-    --                 },
-    --             },
-    --         })
-    --     end
-    -- },
+    {
+        "sindrets/diffview.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        config = function()
+            require("diffview").setup({
+                keymaps = require("usr.keymap").get_diffview_keymaps(),
+                file_panel = {
+                    win_config = {
+                        position = "left",
+                        width = 70,
+                        win_opts = {},
+                    },
+                },
+            })
+        end
+    },
     -- {
     --     "NeogitOrg/neogit",
     --     lazy = true,
