10e59884e0daf34345f4b97ddc17f6b38d6b4df4
eumis <eumisdev@proton.me>
2026-03-26 20:30:18 +0100

    added steam

--- install/cachy/run.sh
+++ install/cachy/run.sh
@@ -51,3 +51,7 @@ sudo pacman -S keepassxc
 
 # https://rust-lang.org/tools/install/
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# gaming
+sudo pacman -S cachyos-gaming-meta
+sudo pacman -S steam
