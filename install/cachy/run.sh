41a43e1ee9f7fc512d9886eac202ef193d1d442e
eumis <eumisdev@proton.me>
2026-04-06 18:26:17 +0200

    updates

--- install/cachy/run.sh
+++ install/cachy/run.sh
@@ -23,46 +23,46 @@ nopm install nerdfonts
 # terminal
 nopm install kitty
 
-sudo pacman -S zoxide
+sudo pacman -S zoxide --noconfirm
 echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 echo "zoxide init fish | source" >> ~/.config/fish/config.fish
 
-sudo pacman -S yazi
+sudo pacman -S yazi --noconfirm
 
 # dev tools
-sudo pacman -S lazygit
-sudo pacman -S wl-clipboard
-sudo pacman -S docker docker-compose
+sudo pacman -S lazygit --noconfirm
+sudo pacman -S wl-clipboard --noconfirm
+sudo pacman -S docker docker-compose --noconfirm
 
 nopm install nvim
 
-sudo pacman -S sqlitebrowser
+sudo pacman -S sqlitebrowser --noconfirm
 nopm install usql
 
 # apps
 nopm install cuplay
 nopm install telegram
-nopm install discord
+# nopm install discord
 nopm install yt-dlp
 
-sudo pacman -S brave-bin
-sudo pacman -S chromium
-sudo pacman -S cava
-sudo pacman -S keepassxc
-sudo pacman -S spotify-launcher
-sudo pacman -S proton-vpn-gtk-app
-sudo pacman -S libreoffice-still
+sudo pacman -S brave-bin --noconfirm
+# sudo pacman -S chromium --noconfirm
+sudo pacman -S cava --noconfirm
+sudo pacman -S keepassxc --noconfirm
+sudo pacman -S spotify-launcher --noconfirm
+sudo pacman -S proton-vpn-gtk-app --noconfirm
+sudo pacman -S libreoffice-still --noconfirm
 
 # https://rust-lang.org/tools/install/
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 nopm install go
 
 # gaming
-sudo pacman -S cachyos-gaming-meta
-sudo pacman -S steam
+sudo pacman -S cachyos-gaming-meta --noconfirm
+sudo pacman -S steam --noconfirm
 
 # virtualbox
-sudo pacman -S virtualbox
-sudo pacman -S virtualbox-guest-utils
-sudo pacman -S virtualbox-guest-iso
-sudo pacman -S virtualbox-host-dkms
+sudo pacman -S virtualbox --noconfirm
+sudo pacman -S virtualbox-guest-utils --noconfirm
+sudo pacman -S virtualbox-guest-iso --noconfirm
+sudo pacman -S virtualbox-host-dkms --noconfirm
