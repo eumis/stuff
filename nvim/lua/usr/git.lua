8fc2dab88ddbeeb0ebf42cc0a4f1eba50cf06691
eumis <eumisdev@proton.me>
2026-06-28 12:12:45 +0200

    used wl-paste

--- nvim/lua/usr/git.lua
+++ nvim/lua/usr/git.lua
@@ -16,13 +16,19 @@ end
 
 local copy_file_path_key = vim.api.nvim_replace_termcodes('<C-o>', true, false, true)
 
+local paste_cmd = { 'xclip', '-selection', 'clipboard', '-o' }
+if vim.fn.executable('wl-paste') == 1 then
+    paste_cmd = { 'wl-paste' }
+end
+
 M.go_to_file = function()
     if state == nil then return end
 
     vim.api.nvim_feedkeys(copy_file_path_key, "t", false)
 
     vim.schedule(function()
-        local path = vim.fn.system({ 'xclip', '-selection', 'clipboard', '-o' })
+        local path = vim.fn.system(paste_cmd)
+        print("pasted path " .. path)
         if vim.uv.fs_stat(path) then
             terminal.close_terminal(state)
             local bufnr = vim.fn.bufnr(path, true)
